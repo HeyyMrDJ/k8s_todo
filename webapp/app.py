7a2d580637a392f018842243b567a3e77c25eaea
DJ <heyymrdj@protonmail.com>
2023-03-19 23:55:18 -0400

    Add todo button now updates k8s

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,7 @@
-"""Python Flask app that will query the kubernetes API for the todoitems 
+"""Python Flask app that will query the kubernetes API for the todoitems
 and display on a kanban according to their lane annotation"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from kubernetes import client, config
 
 
@@ -13,21 +13,43 @@ config.load_kube_config()
 
 
 def return_items():
-    """Query kubernetes API for todoitems and return dictionary of items and their lanes"""
+    """Query kubernetes API for todoitems
+    and return dictionary of items and their lanes"""
     todo_list = []
     items = client.CustomObjectsApi().list_namespaced_custom_object(
             group="kopf.dev",
-            version="v1", 
-            namespace="default", 
+            version="v1",
+            namespace="default",
             plural="todoitems")
 
     for item in items['items']:
-        todo_list.append((item['metadata']['name'], item['metadata']['annotations']['lane']))
+        todo_list.append((
+            item['metadata']['name'],
+            item['metadata']['annotations']['lane']))
 
     return {k: v for k, v in todo_list}
 
 
 @app.route("/")
-def hello_world():
+def root_path():
     todo_dict = return_items()
+    print(todo_dict)
     return render_template('index.html', items=todo_dict)
+
+
+@app.route("/todo", methods=['POST'])
+def todo():
+    name = request.form.get("tinput")
+    resource = render_template(
+            'todo.tmpl',
+            todo_name=name,
+            todo_lane="todo")
+    my_dict = eval(resource)
+    client.CustomObjectsApi().create_namespaced_custom_object(
+        group="kopf.dev",
+        version="v1",
+        namespace="default",
+        plural="todoitems",
+        body=my_dict,
+    )
+    return redirect(url_for('root_path'))
